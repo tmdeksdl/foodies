15a7533b02403c9b85c0a5fd189b509f32d43c09
jpark <tmdeksdl@gmail.com>
2024-01-17 17:46:33 +0900

    Feat: 데이터베이스 세팅

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import * as data from '../src/data/store_data.json';
+
+const prisma = new PrismaClient();
+
+async function seedData() {
+  data?.['DATA']?.map(async (store) => {
+    const storeData = {
+      phone: store?.tel_no,
+      address: store?.rdn_code_nm,
+      lat: store?.x_cnts,
+      lng: store?.y_dnts,
+      name: store?.upso_nm,
+      category: store?.bizcnd_code_nm,
+      storeType: store?.cob_code_nm,
+      foodCertificationName: store?.crtfc_gbn_nm,
+    };
+    const res = await prisma.store.create({
+      data: storeData,
+    });
+  });
+}
+
+async function main() {
+  await seedData();
+}
+
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect;
+  });
